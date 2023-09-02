5009ff70e1c92afdab239d68bf1eafe99545301a
Goonco <s07019@naver.com>
2023-09-03 00:22:54 +0900

    :lipstick: sidebar animation :bento: update fonts

--- src/pages/Map/KakaoMap.jsx
+++ src/pages/Map/KakaoMap.jsx
@@ -14,8 +14,10 @@ const KakaoMap = () => {
 
   const [placeId, setPlaceId] = useState(null);
   const [data, setData] = useState(null);
-  console.log(data);
   const [loading, setLoading] = useState(true);
+  const [sidebarLoading, setSidebarLoading] = useState(true);
+  const [change, setChange] = useState(false);
+  console.log(change);
 
   const getData = async () => {
     const url = BASE_URL + '/buildings';
@@ -50,6 +52,8 @@ const KakaoMap = () => {
       marker.setTitle(it.id);
       kakao.maps.event.addListener(marker, 'click', function (event) {
         setPlaceId(marker.getTitle());
+        setSidebarLoading(false);
+        setChange((prev) => !prev);
       });
     });
   }, [data]);
@@ -60,18 +64,24 @@ const KakaoMap = () => {
       <MapWrapper>
         {/* <button ref={$getInfoButton}>지도 정보 갖고오기</button> */}
         <MapContainer ref={$mapContainer} />
-        <Sidebar id={placeId} data={data} />
+        <Sidebar
+          loading={sidebarLoading}
+          data={data[placeId - 1]}
+          change={change}
+        />
       </MapWrapper>
     );
 };
 
 const MapWrapper = styled.div`
-  display: flex;
+  z-index: 2;
+  position: relative;
 `;
 
 const MapContainer = styled.div`
-  width: 1512px;
-  height: 982px;
+  width: 100vw;
+  height: 100vh;
+  z-index: -1;
 `;
 
 export default KakaoMap;
